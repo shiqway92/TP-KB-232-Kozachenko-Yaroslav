38ba808759916cacfb497c2f04eb5abd4b476434
shiqway92 <144626412+shiqway92@users.noreply.github.com>
2025-01-08 13:13:49 +0200

    Delete Новая папка (2) directory

--- Новая папка (2)/topic_01/task1_reverse_string.py
+++
@@ -1,8 +0,0 @@
-
-def reverse_string(s):
-    return s[::-1]
-
-if __name__ == "__main__":
-    test_string = "Hello, world!"
-    print(f"Original: {test_string}")
-    print(f"Reversed: {reverse_string(test_string)}")
